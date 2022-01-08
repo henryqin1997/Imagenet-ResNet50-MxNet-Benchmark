700830953f9237a1a7a3540b6954c1c7eaccdbb1
Ziheng Qin <37519855+henryqin1997@users.noreply.github.com>
2022-01-08 22:09:34 +0800

    one line to run

---
+++ benchmarks/resnet/implementations/1_node_8_A100_NGC21.05_MXNet/my_run.sh
@@ -0,0 +1,6 @@
+source config_DGXA100.sh
+export CONT=mlperf-nvidia:image_classification
+export DATADIR=/nvme_data/datasets/ILSVRC2012/mxnet_processed_dataset/
+export LOGDIR=/home/${USER}/log
+export SEED=65535 # optional
+bash run_with_docker_once.sh
