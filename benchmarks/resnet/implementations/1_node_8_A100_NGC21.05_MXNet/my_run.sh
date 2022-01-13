1419f94cdc07bb59262f19dea15ada492d57f3e8
Ziheng Qin <37519855+henryqin1997@users.noreply.github.com>
2022-01-13 11:55:30 +0800

    update myonecycle

--- benchmarks/resnet/implementations/1_node_8_A100_NGC21.05_MXNet/my_run.sh
+++ benchmarks/resnet/implementations/1_node_8_A100_NGC21.05_MXNet/my_run.sh
@@ -1,5 +1,5 @@
 source config_DGXA100_onecycle.sh
-export CONT=mlperf-nvidia:image_classification
+export CONT=nvidia-mlperf:image_classification
 export DATADIR=/nvme_data/datasets/ILSVRC2012/mxnet_processed_dataset/
 export LOGDIR=/home/${USER}/log
 export SEED=65535 # optional
